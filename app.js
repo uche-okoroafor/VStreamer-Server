4a9a14ea20efd2bf610e1ea9ed1e91602d9bae78
uche-okoroafor <ucheokoroafo01@gmil.com>
2022-01-20 10:37:26 +0400

    update

--- app.js
+++ app.js
@@ -20,77 +20,79 @@ const viewerRouter = require('./routes/viewerRouter.js')
 const followRouter = require('./routes/followRouter.js')
 const aboutRouter = require('./routes/aboutRouter.js')
 
-// const { json, urlencoded } = express
+const { json, urlencoded } = express
 
-// connectDB()
-// const app = express()
-// const server = http.createServer(app)
-
-// const io = socketio(server, {
-//   cors: {
-//     origin: '*'
-//   }
-// })
-
-// io.on('connection', socket => {
-//   console.log('connected')
-// })
-
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(logger('dev'))
-// }
-// app.use(json())
-// app.use(urlencoded({ extended: false }))
-// app.use(cookieParser())
-// app.use(express.static(join(__dirname, 'public')))
-
-// app.use((req, res, next) => {
-//   req.io = io
-//   next()
-// })
-
-// app.use('/auth', authRouter)
-// app.use('/users', userRouter)
-// app.use('/video', uploadVideoRouter)
-// app.use('/videos', videosRouter)
-// app.use('/image', imageRouter)
-// app.use('/likes', likesRouter)
-// app.use('/comment', commentRouter)
-// app.use('/views', viewerRouter)
-// app.use('/follow', followRouter)
-// app.use('/about', aboutRouter)
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/client/build')))
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html')
-//   )
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running')
-//   })
-// }
-
-// app.use(notFound)
-// app.use(errorHandler)
-
-// // Handle unhandled promise rejections
-// process.on('unhandledRejection', (err, promise) => {
-//   console.log(`Error: ${err.message}`.red)
-//   // Close server & exit process
-//   server.close(() => process.exit(1))
-// })
-
-// module.exports = { app, server }
-
+connectDB()
 const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const io = socketio(server, {
+  cors: {
+    origin: '*'
+  }
 })
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+io.on('connection', socket => {
+  console.log('connected')
 })
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'))
+}
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+  req.io = io
+  next()
+})
+
+app.use('/auth', authRouter)
+app.use('/users', userRouter)
+app.use('/video', uploadVideoRouter)
+app.use('/videos', videosRouter)
+app.use('/image', imageRouter)
+app.use('/likes', likesRouter)
+app.use('/comment', commentRouter)
+app.use('/views', viewerRouter)
+app.use('/follow', followRouter)
+app.use('/about', aboutRouter)
+app.use('/check', (req, res) => {
+  res.json({ say: 'hello world' })
+})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html')
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
+
+module.exports = { app, server }
+
+// const app = express()
+// const port = process.env.PORT || 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`App listening at http://localhost:${port}`)
+// })
